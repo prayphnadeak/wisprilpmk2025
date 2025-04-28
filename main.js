b3df2fb6bd4e74c364b75de88fc394355928c520
prayphnadeak <126833163+prayphnadeak@users.noreply.github.com>
2025-04-28 07:23:29 +0700

    main.js

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 window.onload = function() {
-    window.alert("Harap sabar menunggu. Orang sabar disayang Tuhan :) ");
+  window.alert("Harap sabar menunggu. Orang sabar disayang Tuhan :) ");
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementsByClassName('open')[0].addEventListener('click', function () {
-      document.getElementsByClassName('card-packaging')[0].classList.add('is-open')
-    })
-  
-    document.getElementsByClassName('close')[0].addEventListener('click', function () {
-      document.getElementsByClassName('card-packaging')[0].classList.remove('is-open')
-    })
+  const openButton = document.getElementsByClassName('open')[0];
+  const closeButton = document.getElementsByClassName('close')[0];
+  const cardPackaging = document.getElementsByClassName('card-packaging')[0];
+  const myAudio = document.getElementById('myAudio'); // ambil audio player
+
+  openButton.addEventListener('click', function () {
+      cardPackaging.classList.add('is-open');
+      myAudio.play(); // play musik saat tombol open diklik
   });
 
-
-
+  closeButton.addEventListener('click', function () {
+      cardPackaging.classList.remove('is-open');
+  });
+});
